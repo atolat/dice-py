c5265908c387c5bb20492060fe75703fae76919c
atolat <arpantolat30@gmail.com>
2022-10-31 22:57:08 -0700

    Implement a synchronous TCP server

--- main.py
+++ main.py
@@ -2,14 +2,6 @@
 import argparse
 from server import sync_tcp_server
 import socket
-# Create the parser
-# parser = argparse.ArgumentParser()
-# Add an argument
-# parser.add_argument('--name', type=str, required=True)
-# Parse the argument
-# args = parser.parse_args()
-# Print "Hello" + the user input argument
-# print(sync_tcp_server.test())
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
