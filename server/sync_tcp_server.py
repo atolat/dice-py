182a804443e4fb0fcd9570c8438c4ef1725289ff
atolat <arpantolat30@gmail.com>
2022-10-31 22:19:33 -0700

    Implement a synchronous TCP server

---
+++ server/sync_tcp_server.py
@@ -0,0 +1,33 @@
+import socket
+
+class SyncTCPServer:
+    def __init__(self, host: str, port: str):
+        self.host = host
+        self.port = port
+    
+
+    def __read_command(self, c: socket):
+        data = c.recv(1024)
+        print(f"Received: {data}")
+        return data
+
+    def __respond(self, cmd: str, c: socket):
+        c.sendall(cmd)
+
+    def run_sync_tcp_server(self):
+        print(f"Starting a synchronou TCP server on HOST: {self.host}, PORT: {self.port}")
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    try:
+                        data = self.__read_command(conn)
+                        if not data:
+                            break
+                        self.__respond(data, conn)
+                    except Exception as e:
+                        print(f"Something went wrong -- {e}")
+                        raise e
